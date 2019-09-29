1337f4cd29d8f9512de7ff514e0289a6401b61b5
Ritesh <ritesh.nagpure@rksv.in>
2019-09-29 13:30:44 +0530

    added access control in server

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const baseUrl = "/project/roman-number";
 const conversionController = require("./controller/conversionController");
 const port = process.env.PORT || 8000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get(`${baseUrl}/ping`, (req, res) => {
   res.send("Pong");
 });
