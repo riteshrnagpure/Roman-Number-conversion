aba270476a2d64e286fc644ea618fef634b6d9c5
Ritesh <ritesh.nagpure@rksv.in>
2019-09-29 11:47:50 +0530

    changed port number

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const baseUrl = "/project/roman-number";
 const conversionController = require("./controller/conversionController");
+const port = process.env.PORT || 8000;
 
 app.get(`${baseUrl}/ping`, (req, res) => {
   res.send("Pong");
@@ -11,7 +12,7 @@ app.get(`${baseUrl}/conversion/:input`, (req, res) => {
   conversionController.convertNumber(req, res);
 });
 
-const server = app.listen(8000, function() {
+const server = app.listen(port, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
